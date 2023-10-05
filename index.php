e8e516dc431f0aaf621da45b3c21967cba20a64c
Ojwang1 <wayanicholas@gmail.com>
2023-10-05 12:37:11 +0300

    initial commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Learn PHP</title>
+</head>
+<body>
+    <form action="./send.php" method="post">
+        <input type="number" name="Fnumber" id="" placeholder="Enter your first number">
+        <br>
+        <input type="submit" value="Send" name="send">
+
+    </form>
+    
+</body>
+</html>
---
+++ send.php
@@ -0,0 +1,15 @@
+<?php 
+if(isset($_GET['Fnumber'])){
+    echo "button is clicked";
+}else{
+    echo "button is not clicked";
+}
+
+
+
+// NB This is a testing code
+
+// $Fnumber =$_GET['$Fnumber'];
+// echo $Fnumber;
+
+?>
